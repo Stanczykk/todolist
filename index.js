0f2681b8c2cd2c49d6e891dc94f36ef4d2899528
Stanczyk <stanczyk30@gmail.com>
2025-01-03 18:32:16 +0800

    able to read todo-list.txt

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 import express from "express"
 import bodyParser from "body-parser"
 import ejs from "ejs"
-import { dirname } from "path"
-import { fileURLToPath } from "url"
+import fs from "fs"
+// import { dirname } from "path"
+// import { fileURLToPath } from "url"
 
 // server variables
 const app = express();
 const port = 3000;
+const data = fs.readFileSync("./todo-list.txt").toString().split("\n");
+
+console.log(data.length);
 
 // get current working  directory
-const __dirname = dirname(fileURLToPath(
-    import.meta.url));
+// const __dirname = dirname(fileURLToPath(
+// import.meta.url));
 
 // parses the data for js to understand
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", { tasks: data })
 });
 
 // Server up
 app.listen(port, () => {
-    console.log(`The server is now running at localhost:${port} ${__dirname}`);
+    console.log(`The server is now running at localhost:${port}`);
 });
