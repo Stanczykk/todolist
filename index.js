f9ceadfc08c00f0ab9606b793aad24eb8c11d6da
Stanczyk <stanczyk30@gmail.com>
2025-01-06 19:20:09 +0800

    create and delete tasks

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import fs from "fs"
 
 // server variables
 const app = express();
-const port = 3001;
+const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"))
@@ -28,6 +28,41 @@ app.post("/add", (req, res) => {
     res.redirect("/")
 
 });
+
+app.post("/delete", (req, res) => {
+    const tasksToDelete = req.body.tasksToDelete; // Array of tasks to delete
+
+    if (!tasksToDelete) {
+        return res.redirect('/'); // No tasks selected, just redirect back
+    }
+
+    fs.readFile("./todo-list.txt", 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return res.status(500).send('Failed to load tasks');
+        }
+
+        // Get current tasks
+        const tasks = data.split('\n').filter(task => task.trim() !== '');
+
+        // Filter out the tasks to delete
+        const updatedTasks = tasks.filter(task => !tasksToDelete.includes(task));
+
+        // Rewrite the file with the updated tasks
+        fs.writeFile("./todo-list.txt", updatedTasks.join('\n'), (err) => {
+            if (err) {
+                console.error('Error writing to file:', err);
+                return res.status(500).send('Failed to delete tasks');
+            }
+
+            res.redirect('/'); // Redirect back to the list
+        });
+    });
+});
+
+
+
+// });
 
 app.get("/", (req, res) => {
     const data = fs.readFileSync("./todo-list.txt", "utf8"); //.toString().split("\n");
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
-$(".add").click(function() {
-    $("#formadd").toggleClass("hidden");
-});
+// $(".add").click(function() {
+//     // $("#formadd").toggleClass("hidden");
+// });
+
+// $(".delete").click(function() {
+//     let taskList = $(".tasks ul li");
+//     for (let i = 0; i < taskList.length; i++) {
+//         if ($(`#task-${i}`).is(":checked")) {
+//             $(`#taskList-${i}`).remove();
+//         }
+//     }
+// });
